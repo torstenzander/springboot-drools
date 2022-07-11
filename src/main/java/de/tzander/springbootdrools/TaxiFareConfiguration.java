8fd0b440ac2c292d685626e3e757a0fd3396dab5
Torsten Zander <info@tzander.de>
2022-07-11 16:12:32 +0200

    version update spring boot 2.7.0 and drools 7.68

--- src/main/java/de/tzander/springbootdrools/TaxiFareConfiguration.java
+++ src/main/java/de/tzander/springbootdrools/TaxiFareConfiguration.java
@@ -1,8 +1,8 @@
 package de.tzander.springbootdrools;
 
-import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
+import org.kie.api.KieServices;
 import org.kie.api.builder.KieModule;
 import org.kie.api.runtime.KieContainer;
 import org.kie.internal.io.ResourceFactory;
@@ -12,19 +12,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TaxiFareConfiguration {
 
-    private static final String drlFile = "de/tzander/springbootdrools/rules/TAXI_FARE_RULE.drl";
     private static final String excelFile = "de/tzander/springbootdrools/rules/TAXI_FARE_RULE.xlsx";
+    private static final String drlFile = "de/tzander/springbootdrools/rules/TAXI_FARE_RULE.drl";
+
 
     @Bean
     public KieContainer kieContainer() {
-        KieServices kieServices = KieServices.Factory.get();
+        KieServices service = KieServices.Factory.get();
 
-        KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
+        KieFileSystem kieFileSystem = service.newKieFileSystem();
         kieFileSystem.write(ResourceFactory.newClassPathResource(excelFile));
-        KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem);
+        KieBuilder kieBuilder = service.newKieBuilder(kieFileSystem);
         kieBuilder.buildAll();
         KieModule kieModule = kieBuilder.getKieModule();
 
-        return kieServices.newKieContainer(kieModule.getReleaseId());
+        return service.newKieContainer(kieModule.getReleaseId());
     }
 }
